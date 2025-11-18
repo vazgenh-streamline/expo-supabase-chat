5af4f8ce64bf5fc6981aa3f0ecd5656dc28d5e45
vazgenh-streamline <168567669+vazgenh-streamline@users.noreply.github.com>
2025-11-18 10:28:16 +0400

    Create lib/supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co";
+const SUPABASE_ANON_KEY = "YOUR_ANON_KEY";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+  },
+});
